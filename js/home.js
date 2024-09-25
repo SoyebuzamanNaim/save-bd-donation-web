c80adecb431d6da76dbf488249ffa6aa612bc499
SoyebuzamanNaim <soyebuzamannaim@gmail.com>
2024-09-25 08:27:16 +0600

    history section done

--- js/home.js
+++ js/home.js
@@ -44,13 +44,17 @@ document.getElementById("card1-btn").addEventListener("click", function () {
   if (donationAmount === null || !enoughMoney(donationAmount)) {
     return;
   }
-  const para = document.createElement('p');
-  para.innerHTML=`<div
-      class="container mx-auto rounded outline outline-gray-300 shadow-xl my-6 font-bold p-7 space-y-5"
-    >
-      ${donationAmount} Taka is Donated for Flood at Noakhali, Bangladesh
-    </div>`
-    document.getElementById('history-section').appendChild(para)
+  const para = document.createElement("p");
+  
+  let date = new Date(Date.UTC(2012, 11, 12, 3, 0, 0));
+  para.innerHTML = `<div
+        class="container mx-auto rounded outline outline-gray-300 shadow-xl my-6 font-bold p-7 space-y-5"
+      >
+        <p> ${donationAmount} Taka is Donated for Injured People in Quota Movement, Bangladesh <br>
+    ${date} 
+    </p>
+      </div>`;
+  document.getElementById("history-section").appendChild(para);
   const mainBalance = getNumber("main-balance");
   const donationBalance = getNumber("card1-balance");
   document.getElementById("main-balance").innerText =
@@ -58,60 +62,64 @@ document.getElementById("card1-btn").addEventListener("click", function () {
   document.getElementById("card1-balance").innerText =
     donationBalance + donationAmount;
 
-    document.getElementById("pop-congo").classList.remove("hidden").add("flex");
+  document.getElementById("pop-congo").classList.remove("hidden").add("flex");
 });
 
 // for card2
 document.getElementById("card2-btn").addEventListener("click", function () {
-    const donationAmount = getNumberInput("card2-input");
-    if (donationAmount === null || !enoughMoney(donationAmount)) {
-      return;
-    }
-    const para = document.createElement('p');
-    para.innerHTML=`<div
+  const donationAmount = getNumberInput("card2-input");
+  if (donationAmount === null || !enoughMoney(donationAmount)) {
+    return;
+  }
+  const para = document.createElement("p");
+  let date = new Date(Date.UTC(2012, 11, 12, 3, 0, 0));
+  para.innerHTML = `<div
         class="container mx-auto rounded outline outline-gray-300 shadow-xl my-6 font-bold p-7 space-y-5"
       >
-        ${donationAmount} Taka is Donated for famine-2024 at Feni, Bangladesh
-      </div>`
-      document.getElementById('history-section').appendChild(para)
-    const mainBalance = getNumber("main-balance");
-    const donationBalance = getNumber("card2-balance");
-    document.getElementById("main-balance").innerText =
-      mainBalance - donationAmount;
-    document.getElementById("card2-balance").innerText =
-      donationBalance + donationAmount;
-  
-      document.getElementById("pop-congo").classList.remove("hidden");
-  });
+        <p> ${donationAmount} Taka is Donated for Injured People in Quota Movement, Bangladesh <br>
+    ${date} 
+    </p>
+      </div>`;
+  document.getElementById("history-section").appendChild(para);
+  const mainBalance = getNumber("main-balance");
+  const donationBalance = getNumber("card2-balance");
+  document.getElementById("main-balance").innerText =
+    mainBalance - donationAmount;
+  document.getElementById("card2-balance").innerText =
+    donationBalance + donationAmount;
 
-  // for card3
+  document.getElementById("pop-congo").classList.remove("hidden");
+});
+
+// for card3
 document.getElementById("card3-btn").addEventListener("click", function () {
-    const donationAmount = getNumberInput("card3-input");
-    if (donationAmount === null || !enoughMoney(donationAmount)) {
-      return;
-    }
-    const para = document.createElement('p');
-    para.innerHTML=`<div
+  const donationAmount = getNumberInput("card3-input");
+  if (donationAmount === null || !enoughMoney(donationAmount)) {
+    return;
+  }
+  const para = document.createElement("p");
+  let date = new Date(Date.UTC(2012, 11, 12, 3, 0, 0));
+  para.innerHTML = `<div
         class="container mx-auto rounded outline outline-gray-300 shadow-xl my-6 font-bold p-7 space-y-5"
       >
-        ${donationAmount} Taka is Donated for Injured People in Quota Movement, Bangladesh
-      </div>`
-      document.getElementById('history-section').appendChild(para)
-    const mainBalance = getNumber("main-balance");
-    const donationBalance = getNumber("card3-balance");
+        <p> ${donationAmount} Taka is Donated for Injured People in Quota Movement, Bangladesh <br>
+    ${date} 
+    </p>
+      </div>`;
+  document.getElementById("history-section").appendChild(para);
+  const mainBalance = getNumber("main-balance");
+  const donationBalance = getNumber("card3-balance");
 
-    
-    document.getElementById("main-balance").innerText =
-      mainBalance - donationAmount;
-    document.getElementById("card3-balance").innerText =
-      donationBalance + donationAmount;
-  
-  
+  document.getElementById("main-balance").innerText =
+    mainBalance - donationAmount;
+  document.getElementById("card3-balance").innerText =
+    donationBalance + donationAmount;
 
-    document.getElementById("pop-congo").classList.remove("hidden");
-    
-  });
+  document.getElementById("pop-congo").classList.remove("hidden");
+});
 
-  document.getElementById("pop-congo-close").addEventListener('click',function(){
+document
+  .getElementById("pop-congo-close")
+  .addEventListener("click", function () {
     document.getElementById("pop-congo").classList.add("hidden");
-  })
+  });
